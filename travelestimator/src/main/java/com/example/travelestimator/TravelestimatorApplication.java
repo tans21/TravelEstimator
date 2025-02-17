68421f271b27a64124bc659160ffd98ec827661a
tans21 <97345706+tans21@users.noreply.github.com>
2025-02-17 14:41:06 +0530

    chore: initial project setup

---
+++ travelestimator/src/main/java/com/example/travelestimator/TravelestimatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelestimator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelestimatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelestimatorApplication.class, args);
+	}
+
+}
---
+++ travelestimator/src/test/java/com/example/travelestimator/TravelestimatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelestimator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelestimatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
