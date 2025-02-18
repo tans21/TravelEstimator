0683c2f80031271f1938fa24736d14592217eb22
tans21 <97345706+tans21@users.noreply.github.com>
2025-02-18 14:18:05 +0530

    feat: add Home page

---
+++ travel-estimator-frontend/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { TripForm } from "../components/TripForm";
+import { TripResult } from "../components/TripResult";
+
+const Home = () => {
+    const [tripData, setTripData] = useState(null);
+
+    return (
+        <div className="home">
+            <h1>Travel Estimator</h1>
+            <TripForm setTripData={setTripData} />
+            <TripResult tripData={tripData} />
+        </div>
+    );
+};
+
+export default Home;
