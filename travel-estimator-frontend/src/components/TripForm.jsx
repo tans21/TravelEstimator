483b0ce7f7ea5a4dc8185c13c8f8f276a17f28a2
tans21 <97345706+tans21@users.noreply.github.com>
2025-02-18 12:25:56 +0530

    feat: add TripForm component

---
+++ travel-estimator-frontend/src/components/TripForm.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { calculateTripCost } from "../services/tripService";
+
+const TripForm = ({ setTripData }) => {
+    const [source, setSource] = useState("");
+    const [destination, setDestination] = useState("");
+    const [date, setDate] = useState("");
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const result = await calculateTripCost(source,destination,date);
+            setTripData(result);
+        }
+        catch (error) {
+            alert("Failed to calculate trip cost. Check console for details.");
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="trip-form">
+            <input type="text" placeholder="Source" value={source} onChange={(e) => setSource(e.target.value)} required />
+            <input type="text" placeholder="Destination" value={destination} onChange={(e) => setDestination(e.target.value)} required />
+            <input type="date" placeholder={date} onChange={(e) => setDate(e.target.value)} required />
+            <button type="submit">Calculate Cost</button>
+        </form>
+    );    
+};
+export default TripForm;
