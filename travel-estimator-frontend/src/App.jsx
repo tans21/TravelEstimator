4b23a6aa474a2a2eb6b3c595a77a09018a86944c
tans21 <97345706+tans21@users.noreply.github.com>
2025-02-18 14:22:24 +0530

    feat: set up routing in App.jsx

--- travel-estimator-frontend/src/App.jsx
+++ travel-estimator-frontend/src/App.jsx
@@ -1,11 +1,14 @@
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
-    <>
-      
-    </>
-  )
-}
+    <Router>
+      <Route>
+        <Route path="/" element={<Home/>} />
+      </Route>
+    </Router>
+  );
+};
 
-export default App
+export default App;
